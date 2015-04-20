d38188391088e8a4955761eb25d40e07bf38415b
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2015-04-20 10:25:38 +0530

    Solution for assignment 4 - Image captured as thumbnail.

--- app/src/main/java/com/change/kranti/CaptureIssueActivity.java
+++ app/src/main/java/com/change/kranti/CaptureIssueActivity.java
@@ -2,12 +2,19 @@ package com.change.kranti;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import repository.IssueRepository;
 
 public class CaptureIssueActivity extends Activity {
+
+    private static final int IMAGE_CAPTURE = 2827;
     private IssueRepository issueRepository;
 
     @Override
@@ -17,15 +24,34 @@ public class CaptureIssueActivity extends Activity {
         setContentView(R.layout.create);
     }
 
+    public void captureIssueImage(View view) {
+      Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      startActivityForResult(intent, IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      if (requestCode == IMAGE_CAPTURE && resultCode == Activity.RESULT_OK && data != null) {
+
+        showImage((Bitmap)data.getExtras().get("data"));
+        return;
+      }
+      Toast.makeText(this, "Issue Image not captured !!", Toast.LENGTH_LONG).show();
+    }
 
     public void submit(View view) {
-
-        EditText description = (EditText) findViewById(R.id.descriptionText);
-        EditText title = (EditText) findViewById(R.id.titleText);
-        String issueTitle =  title.getText().toString();
-        String issueDescription =  description.getText().toString();
-        issueRepository.createIssue(issueTitle,issueDescription);
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+      EditText description = (EditText) findViewById(R.id.descriptionText);
+      EditText title = (EditText) findViewById(R.id.titleText);
+      String issueTitle =  title.getText().toString();
+      String issueDescription =  description.getText().toString();
+      issueRepository.createIssue(issueTitle,issueDescription);
+      finish();
     }
+
+    private void showImage(Bitmap data) {
+      ImageView issueImageView = (ImageView) findViewById(R.id.capturedImage);
+      issueImageView.setImageBitmap(data);
+    }
+
+
 }
